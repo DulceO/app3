313fa0b26e512182a6c857abc58738dfbda3f3b8
Dulce Glenda Nineth Ortiz Gutiérrez <201800049@colsanjose.edu.gt>
2021-09-02 17:21:42 -0600

    string

---
+++ app3/app3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace app3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter ar;
+
+            ar = File.AppendText("archivo.txt");
+
+            String agenda = " ";
+            Console.Write("Ingrese Nombre: ");
+            agenda = Console.ReadLine() + "*";
+            Console.Write("Ingrese Apellido: ");
+            agenda = agenda + Console.ReadLine() + "*";
+
+            Console.Write("Ingrese Telefono: ");
+            agenda = agenda + Console.ReadLine() + "*";
+            Console.Write("Ingresa Direccion: ");
+            agenda = agenda + Console.ReadLine() + "*";
+            ar.WriteLine(agenda);
+            agenda = " ";
+            Console.WriteLine("Buscar Contacto: ");
+            string buscar = Console.ReadLine();
+            ar.Close();
+
+            StreamReader 1;
+            1 = File.OpenText("archivo.txt");
+
+            string linea;
+            linea = 1.ReadLine();
+            
+        }
+        }
+    }
+
